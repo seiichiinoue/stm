61af15092039837b171bd364310514361660cf8a
Seiichi Inoue <d35inou108@gmail.com>
2020-02-26 15:27:08 +0900

    support directry reading

--- cstm/model.cpp
+++ cstm/model.cpp
@@ -657,9 +657,9 @@ DEFINE_int32(gamma_alpha_a, 5, "params: gamma_alpha_a");
 DEFINE_int32(gamma_alpha_b, 500, "params: gamma_alpha_b");
 DEFINE_int32(ignore_word_count, 1, "number of ignore word");
 DEFINE_int32(epoch, 100, "num of epoch");
-DEFINE_string(data_path, "./data/train/", "directory input data located");
-DEFINE_string(model_path, "./model/cstm.model", "saveplace of model");
-DEFINE_string(vec_path, "./data/vec.bin", "saveplace of pre-trained word vector");
+DEFINE_string(data_path, "../data/processed/", "directory input data located");
+DEFINE_string(model_path, "./cstm.model", "saveplace of model");
+DEFINE_string(vec_path, "./vec.bin", "saveplace of pre-trained word vector");
 
 int main(int argc, char *argv[]) {
     google::InitGoogleLogging(*argv);
@@ -675,18 +675,27 @@ int main(int argc, char *argv[]) {
     trainer.set_ignore_word_count(FLAGS_ignore_word_count);
     // read file
     const char* path = FLAGS_data_path.c_str();
-    DIR *dp;
-    dp = opendir(path);
-    assert (dp != NULL);
-    dirent* entry = readdir(dp);
-    while (entry != NULL){
-        const char *cstr = entry->d_name;
-        string file_path = string(cstr);
-        if (ends_with(file_path, ".txt")) {
-            std::cout << "loading " << file_path << std::endl;
-            int doc_id = trainer.add_document(FLAGS_data_path + file_path);
+    DIR *dp_author; dp_author = opendir(path);
+    assert (dp_author != NULL);
+    dirent* entry_author = readdir(dp_author);
+    while (entry_author != NULL){
+        const char *cstr = entry_author->d_name;
+        string author = string(cstr);
+        string tmp = FLAGS_data_path + author;
+        const char* path_to_file = tmp.c_str();
+        DIR *dp_file; dp_file = opendir(path_to_file);
+        assert(dp_file != NULL);
+        dirent* entry_file = readdir(dp_file);
+        while (entry_file != NULL) {
+            const char *cstr2 = entry_file->d_name;
+            string file_path = string(cstr2);
+            if (ends_with(file_path, ".txt")) {
+                std::cout << "loading " << file_path << std::endl;
+                int doc_id = trainer.add_document(FLAGS_data_path + author + "/" + file_path);
+            }
+            entry_file = readdir(dp_file);
         }
-        entry = readdir(dp);
+        entry_author = readdir(dp_author);
     }
     // read pre-trained vectors
     vector<wstring> vocab;
