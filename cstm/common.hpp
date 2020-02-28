49b7906b8e39040500f54768a6a8f16deeaa61ee
Seiichi Inoue <d35inou108@gmail.com>
2020-02-29 04:57:47 +0900

    added normalized linear kernel

--- cstm/common.hpp
+++ cstm/common.hpp
@@ -2,9 +2,9 @@
 #include <iostream>
 #include <cmath>
 #include "fmath.hpp"
-#define NDIM_D 2                // 文書,単語ベクトルの次元数
-#define SIGMA_U 0.01            // 文書ベクトののランダムウォーク幅
-#define SIGMA_MU 0.01
+#define NDIM_D 300                // 文書,単語ベクトルの次元数
+#define SIGMA_U 0.01            // 意味空間上の文書ベクトののランダムウォーク幅
+#define SIGMA_V 0.01            // スタイル空間上の文書ベクトルのランダムウォーク幅
 #define SIGMA_PHI 0.02          // 単語ベクトルのランダムウォーク幅
 #define SIGMA_ALPHA 0.2         // a0のランダムウォーク幅
 #define GAMMA_ALPHA_A 5         // a0のガンマ事前分布のハイパーパラメータ
@@ -28,6 +28,16 @@ namespace cstm {
             inner += a[i] * b[i];
         }
         return inner;
+    }
+    double normalized_linear(double *a, double *b, int length) {
+        double inner = 0;
+        double a2 = 0, b2 = 0;
+        for (int i=0; i<length; ++i) {
+            inner += a[i] * b[i];
+            a2 += a[i] * a[i];
+            b2 += b[i] * b[i];
+        }
+        return inner / (a2 * b2);
     }
     void dump_vec(double *vec, int len) {
         std::cout << "[";
